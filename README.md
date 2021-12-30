4cc91406f8bc92172771220e4f9bc78c178f2df8
rusty-reebs <rusty_reebs@tutanota.com>
2021-12-30 12:14:27 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Informational Site
+
+A basic informational site consisting of four pages, index, about, contact, and 404.
+
+Built with node.js.
---
+++ index.js
@@ -0,0 +1,40 @@
+// index.js
+
+const http = require("http");
+const fs = require("fs");
+
+http
+  .createServer((req, res) => {
+    if (req.url === "/") {
+      fs.readFile("index.html", (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else if (req.url === "/about") {
+      fs.readFile("about.html", (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else if (req.url === "/contact") {
+      fs.readFile("contact-me.html", (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      fs.readFile("404.html", (err, data) => {
+        if (err) throw err;
+        res.writeHead(404, { "Content-type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    }
+  })
+  .listen(8080, () => {
+    console.log("Listening on port 8080...");
+  });
